383520fd4fd309af6323183d0b65f00034bf3d9b
marta <marta.voitanowych@gmail.com>
2017-05-03 16:02:00 +0300

    add activities, listeners

--- app/src/main/java/com/maltseva/marta/sing/ChooseActivity.java
+++ app/src/main/java/com/maltseva/marta/sing/ChooseActivity.java
@@ -1,13 +1,41 @@
 package com.maltseva.marta.sing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ChooseActivity extends AppCompatActivity {
+import static com.maltseva.marta.sing.R.id.basic_btn;
+
+public class ChooseActivity extends AppCompatActivity implements View.OnClickListener {
+    Button basic;
+    Button medium;
+    Button advanced;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+        initButtons();
+        setListeners();
+    }
+
+    private void initButtons() {
+        basic = (Button) findViewById(basic_btn);
+        medium = (Button) findViewById(R.id.medium_btn);
+        advanced = (Button) findViewById(R.id.adv_btn);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(ChooseActivity.this, PlayActivity.class);
+        startActivity(intent);
+    }
+
+    public void setListeners() {
+        basic.setOnClickListener(this);
+        medium.setOnClickListener(this);
+        advanced.setOnClickListener(this);
     }
 }
---
+++ app/src/main/java/com/maltseva/marta/sing/PlayActivity.java
@@ -0,0 +1,61 @@
+package com.maltseva.marta.sing;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Марта on 03.05.2017.
+ */
+
+public class PlayActivity extends Activity implements View.OnClickListener{
+    TextView song1;
+    TextView song2;
+    TextView song3;
+
+    Button play1;
+    Button play2;
+    Button play3;
+
+    Button stop1;
+    Button stop2;
+    Button stop3;
+    MediaPlayer mp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.play_activity);
+        init();
+        setListeners();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    private void setListeners() {
+        play1.setOnClickListener(this);
+        play2.setOnClickListener(this);
+        play3.setOnClickListener(this);
+        stop1.setOnClickListener(this);
+        stop2.setOnClickListener(this);
+        stop3.setOnClickListener(this);
+    }
+
+    private void init() {
+        song1 = (TextView)findViewById(R.id.first);
+        song2 = (TextView)findViewById(R.id.second);
+        song3 = (TextView)findViewById(R.id.third);
+        play1 = (Button) findViewById(R.id.play1);
+        play2 = (Button) findViewById(R.id.play2);
+        play3 = (Button) findViewById(R.id.play3);
+        stop1 = (Button) findViewById(R.id.stop1);
+        stop2 = (Button) findViewById(R.id.stop2);
+        stop3 = (Button) findViewById(R.id.stop3);
+        mp = new MediaPlayer();
+    }
+}
