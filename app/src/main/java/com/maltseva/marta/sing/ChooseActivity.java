13298aa5e39c15cdd829e2cde967618ae891e639
marta <marta.voitanowych@gmail.com>
2017-05-03 13:57:50 +0300

    Initial commit

---
+++ app/src/main/java/com/maltseva/marta/sing/ChooseActivity.java
@@ -0,0 +1,13 @@
+package com.maltseva.marta.sing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+    }
+}
